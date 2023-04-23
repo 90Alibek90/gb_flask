339c673c1381447a7b6bb92d0c0aeeb2823d14f7
Alibek Sugurbekov <alibek.sugurbek@inbox.ru>
2023-04-23 20:09:59 +0600

    lesson_3

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,31 +1,36 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
 
 user = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 USERS = {
-    1: {"name": "Alibek"},
-    2: {"name": "Dmitriy"},
-    3: {"name": "Maria"}
+    1: {"name": "Ivan"},
+    2: {"name": "Jon"},
+    3: {"name": "Mary"}
 }
 
 
 @user.route("/")
+@login_required
 def user_list():
+    from ..models import User
+    users = User.query.all()
     return render_template(
         "users/list.html",
-        users=USERS
+        users=users
     )
 
 
 @user.route("/<int:pk>")
-def get_user(pk: int):
-    if pk in USERS:
-        user_raw = USERS[pk]
-    else:
+@login_required
+def profile(pk: int):
+    from ..models import User
+    _user = User.query.filter_by(id=pk).one_or_none()
+    if _user is None:
         raise NotFound("User id:{}, not found".format(pk))
     return render_template(
         "users/details.html",
-        user_name=user_raw["name"]
+        user=_user
     )
 
 
