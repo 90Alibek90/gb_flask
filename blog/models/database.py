f72c79aa81673b6eb968b07bbe6d5245b8d07e94
Alibek Sugurbekov <alibek.sugurbek@inbox.ru>
2023-05-09 14:37:42 +0600

    lesson 6

--- blog/models/database.py
+++ blog/models/database.py
@@ -1,4 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
-db = SQLAlchemy()
-__all__ = ["db", ]
+from ..app import db
+
+
+class Author(db.Model):
+    __tablename__ = "authors"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
+
+    user = relationship("User", back_populates="author")
+    articles = relationship("Article", back_populates="author")
